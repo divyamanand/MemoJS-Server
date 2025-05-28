ff7b7f4f28c9a19d5bbd8f02ca588b0a04bb1a89
Divyam Anand <wrkdivan@gmail.com>
2025-05-28 20:28:10 +0530

    Revisions add paused

--- api/addQuestions.js
+++ api/addQuestions.js
@@ -62,12 +62,12 @@ const addQuestions = async (req, res) => {
             const { id, added_on } = insertResult.rows[0];
 
 
-            const revisionDates = calculateRevisionDates(difficulty, added_on);
-            const revisions = [];
-            for (const date of revisionDates) {
-                const r = await revision.query(qAddRevision, [id, date]);
-                revisions.push(r.rows[0]);
-            }
+            // const revisionDates = calculateRevisionDates(difficulty, added_on);
+            // const revisions = [];
+            // for (const date of revisionDates) {
+            //     const r = await revision.query(qAddRevision, [id, date]);
+            //     revisions.push(r.rows[0]);
+            // }
 
             quests.push({ question, revisions });
         }
